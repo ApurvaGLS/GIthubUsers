24f9bdc84899869c239f4053a3d165a9359d3de4
Apurva Dongre <apurva@giantleapsystems.com>
2020-08-19 15:42:29 +0530

    Commit with sqlite

--- MachineTestJio/Github Users Screens/ViewController.swift
+++ MachineTestJio/Github Users Screens/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController,UISearchBarDelegate{
     var offlineArray : [GithubUserBean]?
     var userArr : NSArray?
     var myTableView: UITableView!
+    var db : OpaquePointer?
+    var fileUrl : URL?
     let searchController = UISearchController(searchResultsController: nil)
 
     override func viewDidLoad() {
@@ -27,78 +29,150 @@ class ViewController: UIViewController,UISearchBarDelegate{
         setUpNavigationBar()
         setUpTable()
         setUpSearchBar()
-        if(Reachability.isConnectedToNetwork()){
-            print("Internet Connection Available")
-            loadData()
-        }else{
-            print("No Internet Connection Available")
-        }
         
+        fileUrl = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        .appendingPathComponent("GithubDatabase.sqlite")
+
         if UIApplication.isFirstLaunch() {
-            print("First Time ")
+            openDatabase()
+            createTable()
+            if(Reachability.isConnectedToNetwork()){
+                loadData()
+            }else{
+                let alert = UIAlertController.init(title: "Alert", message: "Make sure you have a worrking internet connection", preferredStyle: .alert)
+                alert.addAction(UIAlertAction.init(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
         }else{
-            print("Not first time")
-        }
-    }
-    
-    func setUpNavigationBar(){
-        self.title = "Github Users"
-        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
-        navigationController?.navigationBar.barTintColor = .systemPink
-    }
-    
-    func setUpTable(){
-        let displayWidth: CGFloat = self.view.frame.width
-        let displayHeight: CGFloat = self.view.frame.height
-        myTableView = UITableView(frame: CGRect(x: 0, y: 0, width: displayWidth, height: displayHeight))
-        myTableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: displayWidth, height: 50))
-        myTableView.contentInset = UIEdgeInsets(top: 0,left: 0,bottom: 0,right: 0);
-        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
-        myTableView.dataSource = self
-        myTableView.delegate = self
-        self.view.addSubview(myTableView)
-    }
-    
-    func setUpSearchBar(){
-        searchController.searchResultsUpdater = self
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.placeholder = "Search Github User"
-        definesPresentationContext = true
-        myTableView.tableHeaderView = searchController.searchBar
-    }
-    
-    func loadData() {
-        let appUrl = "https://api.github.com/search/users?q=torvalds&page=1"
-        get(url: appUrl) { (success, response) in
-            if success {
-               weak var weakSelf = self
-                if let jsonResult = response as? [String : Any] { //since there's array in API
-                    weakSelf?.parseJsonResponse(dict: jsonResult)
-                } else {
-                    print("Erroer while parsing the data")
-                }
+            if(Reachability.isConnectedToNetwork()){
+                loadData()
+            }else{
+                print("No Internet Connection Available")
             }
         }
     }
     
-    func parseJsonResponse(dict:[String : Any]){
-        
-        DispatchQueue.main.async {
-            self.githubuserArray!.removeAll()
-            self.myTableView.reloadData()
-        }
-        userArr = (dict["items"] as? NSArray)!
-        for arrDict in userArr! {
-            let bean = GithubUserBean().initWithObject(dict: arrDict as! NSDictionary)
-            githubuserArray!.append(bean as! GithubUserBean)
-        }
-        DispatchQueue.main.async {
-            self.myTableView.reloadData()
+    
+  func setUpNavigationBar(){
+      self.title = "Github Users"
+      navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+      navigationController?.navigationBar.barTintColor = .systemPink
+  }
+  
+  func setUpTable(){
+      let displayWidth: CGFloat = self.view.frame.width
+      let displayHeight: CGFloat = self.view.frame.height
+      myTableView = UITableView(frame: CGRect(x: 0, y: 0, width: displayWidth, height: displayHeight))
+      myTableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: displayWidth, height: 50))
+      myTableView.contentInset = UIEdgeInsets(top: 0,left: 0,bottom: 0,right: 0);
+      myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+      myTableView.dataSource = self
+      myTableView.delegate = self
+      self.view.addSubview(myTableView)
+  }
+  
+  func setUpSearchBar(){
+      searchController.searchResultsUpdater = self
+      searchController.obscuresBackgroundDuringPresentation = false
+      searchController.searchBar.placeholder = "Search Github User"
+      definesPresentationContext = true
+      myTableView.tableHeaderView = searchController.searchBar
+  }
+
+func loadData() {
+    let appUrl = "https://api.github.com/search/users?q=torvalds&page=1"
+    get(url: appUrl) { (success, response) in
+        if success {
+           weak var weakSelf = self
+            if let jsonResult = response as? [String : Any] { //since there's array in API
+                weakSelf?.parseJsonResponse(dict: jsonResult)
+            } else {
+                print("Erroer while parsing the data")
+            }
         }
     }
 }
 
-extension ViewController : UITableViewDelegate, UITableViewDataSource{  
+func parseJsonResponse(dict:[String : Any]){
+    DispatchQueue.main.async {
+        self.githubuserArray!.removeAll()
+        self.myTableView.reloadData()
+    }
+    userArr = (dict["items"] as? NSArray)!
+    for arrDict in userArr! {
+        let bean = GithubUserBean().initWithObject(dict: arrDict as! NSDictionary)
+        githubuserArray!.append(bean as! GithubUserBean)
+    }
+
+    DispatchQueue.main.async {
+        self.myTableView.reloadData()
+    }
+}
+    
+    
+func openDatabase(){
+     if sqlite3_open(fileUrl?.path, &db) != SQLITE_OK {
+        print("error opening database")
+    }
+}
+
+func createTable(){
+    if sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS GithubUsers ('login' TEXT, 'avatarUrl' TEXT, 'followersUrl' TEXT)", nil, nil, nil) != SQLITE_OK {
+        let errmsg = String(cString: sqlite3_errmsg(db)!)
+        print("error creating table: \(errmsg)")
+    }else{
+       print("Table Created Successfully")
+    }
+}
+ 
+func insertIntoTable(){
+    for i in 0..<githubuserArray!.count{
+        let login = githubuserArray![i].login
+        let avatarUrl = githubuserArray![i].avatar_url
+        let followersUrl = githubuserArray![i].followers_url
+        
+        let queryString = "INSERT INTO GithubUsers ('login','avatarUrl','followersUrl') VALUES ('\(login!)','\(avatarUrl!)','\(followersUrl!)')"
+        
+        if sqlite3_exec(db, queryString, nil, nil, nil) != SQLITE_OK {
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("NOT OK: \(errmsg)")
+        }else{
+            print("OK")
+        }
+    }
+//    readValues()
+}
+    
+func readValues(){
+    self.githubuserArray!.removeAll()
+
+   let queryString = "SELECT * FROM GithubUsers"
+
+    var stmt:OpaquePointer?
+
+   if sqlite3_prepare(db, queryString, -1, &stmt, nil) != SQLITE_OK{
+       let errmsg = String(cString: sqlite3_errmsg(db)!)
+       print("error preparing select: \(errmsg)")
+       return
+   }
+
+   while(sqlite3_step(stmt) == SQLITE_ROW){
+       let login = String(cString: sqlite3_column_text(stmt, 0))
+       let avatarUrl = String(cString: sqlite3_column_text(stmt, 1))
+       let followersUrl = String(cString: sqlite3_column_text(stmt, 2))
+
+      let bean = GithubUserBean()
+      bean.login = login
+      bean.avatar_url = avatarUrl
+      bean.followers_url = followersUrl
+
+      githubuserArray?.append(bean)
+   }
+    print("SQLITE ARRAY COUNT \(githubuserArray!.count)")
+}
+}
+
+extension ViewController : UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if(searchController.isActive){
             return filteredgithubuserArray!.count
